469efa79892f04e2f8ab8fe63c7a4aa67af24284
luirusso <luirusso_@hotmail.it>
2021-12-09 17:54:00 +0100

    added style to dynamic cards inside main

---
+++ src/components/Card.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="px-3">
+        <div class="pb-3">
+            <img :src="require(`../assets/${card_image}`)" alt="" />
+        </div>
+        <div class="fs-4">{{ card_title }}</div>
+        <div class="d-flex justify-content-center">
+            <div>{{ card_author }}</div>
+            <div class="px-1">|</div>
+            <div>{{ card_date }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Card",
+    props: {
+        card_image: String,
+        card_title: String,
+        card_author: String,
+        card_date: String,
+    },
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,6 @@
 <template>
     <main>
-        <div class="container position-relative">
+        <div class="container pb-5 position-relative">
             <div
                 class="d-flex pb-4 justify-content-space-between align-items-center"
             >
@@ -10,9 +10,9 @@
                 </div>
                 <hr />
             </div>
-            <div class="site_card d-flex text-center">
+            <div class="site_card d-flex text-center justify-content-center">
                 <!-- CARD -->
-                <div>
+                <!-- <div>
                     <div class="pb-3">
                         <img
                             src="../assets/single-post-img3-400x263.jpg"
@@ -27,6 +27,18 @@
                         <div>|</div>
                         <div>March 25th, 2019</div>
                     </div>
+                </div> -->
+
+                <div
+                    v-for="(element, index) in foodieJournalArray"
+                    :key="`foodieJournal-${index}`"
+                >
+                    <Card
+                        :card_image="element.card_image"
+                        :card_title="element.card_title"
+                        :card_author="element.card_author"
+                        :card_date="element.card_date"
+                    />
                 </div>
             </div>
         </div>
@@ -34,20 +46,28 @@
 </template>
 
 <script>
+import Card from "./Card.vue";
+
 export default {
     name: "Main",
+    components: {
+      Card
+    },
+
     data() {
         return {
             foodieJournalArray: [
                 {
                     card_image: "single-post-img3-400x263.jpg",
-                    card_title: "Food Corner: Top Japanese Restaurants for Sushi",
+                    card_title:
+                        "Food Corner: Top Japanese Restaurants for Sushi",
                     card_author: "By admin",
                     card_date: "March 25th, 2019",
                 },
                 {
                     card_image: "fi-roundup-400x263.jpg",
-                    card_title: "Roundup: My New Favourite Recipes For Healthy Living",
+                    card_title:
+                        "Roundup: My New Favourite Recipes For Healthy Living",
                     card_author: "By admin",
                     card_date: "March 25th, 2019",
                 },
@@ -76,7 +96,4 @@ hr {
     top: -70px;
 }
 
-.site_card {
-    width: calc(100% / 3);
-}
 </style>
