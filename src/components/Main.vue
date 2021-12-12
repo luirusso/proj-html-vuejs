588b67c900f85bc17030659895453d03e533b108
luirusso <luirusso_@hotmail.it>
2021-12-12 14:29:22 +0100

    added side widget for recent and popular posts

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -274,7 +274,7 @@
                             Follow Us
                         </div>
                     </div>
-                    <div class="social-wrapper">
+                    <div class="social-wrapper pb-5">
                         <div class="d-flex">
                             <i
                                 v-for="(social, index) in socialCubesArray"
@@ -284,6 +284,31 @@
                             >
                             </i>
                         </div>
+                    </div>
+                    <div class="col-12 pb-5">
+                        <div class="row">
+                            <div
+                                class="col-6 side-post-title text-center title-font white d-flex align-items-center justify-content-center cursor-pointer text-glow"
+                            >
+                                Popular
+                            </div>
+                            <div
+                                class="col-6 side-post-title text-center title-font d-flex align-items-center justify-content-center cursor-pointer text-glow"
+                            >
+                                Recent
+                            </div>
+                        </div>
+                    </div>
+                    <div
+                        v-for="(post, index) in postsWidgetArray"
+                        :key="`post-${index}`"
+                        class="col-12"
+                    >
+                        <PostsWidget
+                            :image="post.image"
+                            :title="post.title"
+                            :date="post.date"
+                        />
                     </div>
                 </div>
             </div>
@@ -296,6 +321,7 @@ import Card from "./Card.vue";
 import SectionDivider from "./SectionDivider.vue";
 import HoverLink from "./HoverLink.vue";
 import CulinaryCollections from "./CulinaryCollections.vue";
+import PostsWidget from "./PostsWidget.vue";
 
 export default {
     name: "Main",
@@ -304,6 +330,7 @@ export default {
         SectionDivider,
         HoverLink,
         CulinaryCollections,
+        PostsWidget,
     },
 
     data() {
@@ -451,6 +478,23 @@ export default {
                     icon: "fab fa-pinterest-p",
                 },
             ],
+            postsWidgetArray: [
+                {
+                    image: "single-post-img3-66x66.jpg",
+                    title: "Food Corner: Top Japanes Restaurants for Sushi",
+                    date: "March 25th, 2019",
+                },
+                {
+                    image: "singapore-featured-image-66x66.jpg",
+                    title: "City Guide: Singapore",
+                    date: "February 27th, 2019",
+                },
+                {
+                    image: "slide1-bg-66x66.jpg",
+                    title: "6 Nutritional Tips to Help Burn Down Body Fat",
+                    date: "February 28th, 2019",
+                },
+            ],
         };
     },
 };
@@ -540,5 +584,10 @@ hr {
     width: 35px;
     border-radius: 5px;
     border: 0.5px solid grey;
+}
+
+.side-post-title {
+    height: 50px;
+    border: 0.5px solid lightgray;
 }
 </style>
---
+++ src/components/PostsWidget.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="d-flex align-items-center pb-5">
+        <div class="pe-3">
+            <img
+                class="round cursor-pointer"
+                :src="require(`../assets/${image}`)"
+                :alt="title"
+            />
+        </div>
+        <div>
+            <div class="fs-6 cursor-pointer text-glow">
+                {{title}}
+            </div>
+            <div class="date">{{date}}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PostsWidget',
+    props: {
+        image: String,
+        title: String,
+        date: String,
+    }
+};
+</script>
+
+<style scoped lang="scss">
+@import "../styles/globals.scss";
+.date {
+    color: $small-text-color;
+}
+
+img {
+    max-width: 55px;
+}
+</style>
