cc1298f32c8ea3e7c699f0599050b47a6f5c895d
luirusso <luirusso_@hotmail.it>
2021-12-12 16:38:21 +0100

    completed styling twitter widget

--- src/components/TweetsWidget.vue
+++ src/components/TweetsWidget.vue
@@ -3,25 +3,80 @@
         <div class="tweets-wrapper p-4">
             <div class="d-flex justify-content-between pb-4">
                 <div>
-                    <span>Tweets </span> <span>by</span>
-                    <span>@Theme_Fusion</span>
+                    <span class="fs-4">Tweets </span> <span>by </span>
+                    <a href="#"><span>@Theme_Fusion</span></a>
                 </div>
                 <div>
-                    <img src="../assets/image.svg" alt="" />
+                    <img class="cursor-pointer" src="../assets/image.svg" alt="" />
                 </div>
             </div>
-            <div class="d-flex justify-content-between">
-                <div class="d-flex align-items-center">
-                    <img class="pe-2"
-                        src="../assets/3a74ce3d0532b7773b174c45ca3bd05a_normal.png"
-                        alt=""
-                    />
-                    <div class="d-flex flex-column">
-                        <span>ThemeFusion</span> <span>@Theme_Fusion</span>
+
+            <div class="row">
+                <div class="col-2">
+                    <div class="d-flex justify-content-between">
+                        <div class="d-flex align-items-center">
+                            <img
+                                class="pe-1 cursor-pointer"
+                                src="../assets/3a74ce3d0532b7773b174c45ca3bd05a_normal.png"
+                                alt=""
+                            />
+                        </div>
                     </div>
                 </div>
-                <div>
-                    <img class="twitter-icon" src="../assets/image (1).svg" alt="">
+                <div class="col-10">
+                    <div class="d-flex justify-content-between">
+                        <div class="d-flex flex-column">
+                            <span class="fw-bold">ThemeFusion</span>
+                            <a href="#"><span class="grey-link">@Theme_Fusion</span></a>
+                        </div>
+                        <div>
+                            <img
+                                class="twitter-icon cursor-pointer"
+                                src="../assets/image (1).svg"
+                                alt=""
+                            />
+                        </div>
+                    </div>
+                    <div class="pb-4">
+                        Do you need sublime WordPress hosting for your next
+                        website? Take advantage of exclusive partner offers that
+                        we have secured just for you & launch your site in
+                        seconds with
+                        <span>#avada</span> on WP Engine hosting & get 30% Off
+                        this Black Friday <a href="#">bit.ly/3kjLLE2</a>
+                        <span> <a href="#"> #BlackFriday2020</a></span>
+                    </div>
+                    <div class="pb-2">
+                        <img
+                            class="post-img-content cursor-pointer w-100"
+                            src="../assets/En2TRxLW4AEiWUN.jpeg"
+                            alt=""
+                        />
+                    </div>
+                    <div>
+                        <div></div>
+                        <div class="d-flex justify-content-between">
+                            <div class="d-flex">
+                                <div class="pe-3">
+                                    <img
+                                        class="w-75 cursor-pointer"
+                                        src="../assets/image (2).svg"
+                                        alt=""
+                                    />
+                                </div>
+                                <div>
+                                    <img
+                                        class="w-75 cursor-pointer"
+                                        src="../assets/image (3).svg"
+                                        alt=""
+                                    />
+                                </div>
+                            </div>
+                            <div>
+                                <span class="grey-link">21h</span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -35,12 +90,22 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import "../styles/globals.scss";
+
 .tweets-wrapper {
     background-color: white;
     border-radius: 10px;
 }
 
+.grey-link {
+    color: $primary-text-color;
+}
+
 .twitter-icon {
     width: 25px;
+}
+
+.post-img-content {
+    border-radius: 5px;
 }
 </style>
